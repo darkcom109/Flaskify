2e29ac2ff6eac823bed96a2514bd9f2005860b4a
darkcom109 <garciavalentino2006@gmail.com>
2025-09-04 11:13:55 +0100

    Starting to develop the authentication system

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+# Create a form class
+class SignUpForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your email", validators=[DataRequired()])
+    password = StringField("Choose a password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template("signup.html")
 

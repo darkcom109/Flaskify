c1a518195e78ac4b153f838a46d72beca33bef0b
darkcom109 <garciavalentino2006@gmail.com>
2025-09-04 15:06:28 +0100

    Added signup and login authentication, need to still add some test-cases to resolve integrity issues with the database

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from flask_login import UserMixin, LoginManager, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 # Add Database
@@ -17,24 +19,34 @@ app.config['SECRET_KEY'] = os.getenv("FORM_SECRET_KEY")
 db = SQLAlchemy(app)
 
 # Create Model
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
+# Initialise LoginManager
+login_manager = LoginManager(app)
+
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
 # Create a sign-up form class
 class SignUpForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # Create a login form class
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 @app.route('/')
@@ -47,21 +59,37 @@ def signup():
     form = SignUpForm()
 
     if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data).first()
-        if user is None:
-            user = Users(name=form.name.data, email=form.email.data, password=form.password.data)
-            db.session.add(user)
-            db.session.commit()
-        name = form.name.data
+        hashed_pw = generate_password_hash(
+            form.password.data,
+            method="pbkdf2:sha256",
+            salt_length=8
+        )
 
-        form.name.data = ''
-        form.email.data = ''
-        form.password.data = ''
-    return render_template("signup.html", form=form, name=name)
+        user = Users(
+            name=form.name.data, 
+            email=form.email.data, 
+            password=hashed_pw
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+        flash("You have successfully signed up to Flaskify!")
+        return redirect(url_for("login"))
+    
+    return render_template("signup.html", form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("You have successfully logged in!")
+            return redirect(url_for("index"))
+        else:
+            flash("Login failed. Check your username and password")
+        
     return render_template("login.html", form=form)
 
 if __name__ == "__main__":

a20936d3fa2b4f279556c35bbafd69644fa8779b
darkcom109 <garciavalentino2006@gmail.com>
2025-09-10 00:57:25 +0100

    Reversed the order of the posts from oldest to newest

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def delete_post(id):
 @login_required
 def posts():
     # Grab all posts from DB
-    posts = Posts.query.order_by(Posts.date_posted)
+    posts = Posts.query.order_by(Posts.date_posted.desc()).all()
     return render_template("posts.html", posts=posts)
 
 @app.route('/posts/<int:id>')
